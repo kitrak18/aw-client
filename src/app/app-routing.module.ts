4d71eb2079cf4919702fc97c733db0ad1f75d017
Kartik Mathur <kartikmathur96@gmail.com>
2021-08-15 20:30:18 -0400

    Version2.1-Basic Home Page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OrdersComponent } from './orders/orders.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '',redirectTo: 'home',pathMatch: 'full'},
+  // {path:'',component: AppComponent,    },
+  {path:'home',component: HomeComponent,    },
+  // {path:'products',component: ProductsComponent,    },
+  // {path:'smartship',component: SmartshipComponent,    },
+  {path:'orders',component: OrdersComponent,    },
+  // {path:'vendors',component: VendorsComponent,    },
+  // {path:'updateOrder',component: UpdateOrdersComponent,    },
+  // {path:'customers',component: CustomersComponent,    },
+  ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes,{useHash:true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,27 @@
-import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule} from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { OrdersComponent } from './orders/orders.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OrdersComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    HttpClientModule,
+    ReactiveFormsModule,
+    RouterModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
